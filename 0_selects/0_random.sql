9d941bc4d98e6f12969bb4922cca00e68cb93a2f
Asim <asim.awan@outlook.com>
2021-10-07 01:27:18 +0000

    Test code to create new tables

--- 0_selects/0_random.sql
+++ 0_selects/0_random.sql
@@ -1,1 +1,28 @@
-SELECT count(*) FROM assignment_submissions;
+-- SELECT count(*) FROM assignment_submissions;
+-- SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name
+-- HAVING count(assignment_submissions.*) < 100;
+
+
+CREATE TABLE teachers (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR (225) NOT NULL,
+start_date DATE,
+end_date DATE,
+is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE assistance_requests (
+id SERIAL PRIMARY KEY NOT NULL,
+assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+created_at TIMESTAMP,
+started_at TIMESTAMP,
+completed_at TIMESTAMP,
+student_feedback TEXT,
+teacher_feedback TEXT
+);
