91c27fe32ce6e79161adfb0ebb8d613aa5e872bd
Asim <asim.awan@outlook.com>
2021-10-11 02:30:50 +0000

    Add protection from SQL Injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,6 +7,26 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(student => {
+    console.log(`${student.name} has an id of ${student.student_id} and was in the ${student.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
+
 // pool.query(`
 // SELECT id, name, cohort_id
 // FROM students
@@ -16,18 +36,3 @@ const pool = new Pool({
 //   console.log(res.rows);
 // })
 // .catch(err => console.error('query error', err.stack));
-
-
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students 
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(student => {
-    console.log(`${student.name} has an id of ${student.student_id} and was in the ${student.cohort} cohort`);
-  })
-})
-.catch(err => console.error('query error', err.stack));
